c57364102ff48961d17d46d029e73aa5f083cd7d
luccasadriano <luccas-adriano@hotmail.com>
2020-12-03 21:09:23 -0300

    eventos, formulários e criando element

--- js/principal.js
+++ js/principal.js
@@ -37,6 +37,36 @@ for (var i = 0; i < pacientes.length; i++) {
 }
 
 var buttom = document.querySelector("#adicionar-paciente")
-buttom.addEventListener('click', function () {
-    console.log('oi')
+buttom.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    var form = document.querySelector("#form-adiciona")
+    var nome = form.nome.value
+    var peso = form.peso.value
+    var altura = form.altura.value
+    var gordura = form.gordura.value
+    // console.log(nome, peso, altura, gordura)
+
+    var pacienteTr = document.createElement("tr")
+    var nomeTd = document.createElement("td")
+    var pesoTd = document.createElement("td")
+    var alturaTd = document.createElement("td")
+    var gorduraTd = document.createElement("td")
+    var imcTd = document.createElement("td")
+
+    nomeTd.textContent = nome
+    pesoTd.textContent = peso
+    alturaTd.textContent = altura
+    gorduraTd.textContent = gordura
+    // imcTd.textContent = 
+
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+    // pacienteTr.appendChild(imcTd)
+
+  var tabela = document.querySelector("#tabela-pacientes")
+  tabela.appendChild(pacienteTr)
+    
 })
