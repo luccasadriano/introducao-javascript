5234bd5b0d9a6c234f853f70fbb12bdb964b400d
luccasadriano <luccas-adriano@hotmail.com>
2020-12-09 10:54:41 -0300

    Remoção, Delegação e Animação finalizado

--- js/removerPaciente.js
+++ js/removerPaciente.js
@@ -1,13 +1,15 @@
 var pacientes = document.querySelectorAll(".paciente")
 
-
 var tabela = document.querySelector("table")
-tabela.addEventListener("dblclick",function(paciente){
+tabela.addEventListener("dblclick", function (event) {
+   
+   if (event.target.tagName == 'TD') {
 
-   event.target.parentNode.remove()
-      //é
-   // var alvoEvento = event.target
-   // var paiDoAlvo = alvoEvento.parentNode
-   // paiDoAlvo.remove()
+      event.target.parentNode.classList.add("fadeOut")
+
+      setTimeout(function () {
+         event.target.parentNode.remove()
+      }, 700)
+   }
 
 })
